76618f109a9eccb8bf7300d21d2f0743c67d7d45
StefanPenkVivino <stefan.penk@vivino.com>
2024-07-19 16:13:37 +0200

    added gulp-build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
-const {
-  src,
-  dest,
-  watch,
-  series
-} = require('gulp');
+const { src, dest, watch, series } = require("gulp");
 // const sass = require('gulp-sass')(require('sass'));
 // const prefix = require('gulp-autoprefixer');
 // const minify = require('gulp-clean-css');
-const terser = require('gulp-terser');
-const imagemin = require('gulp-imagemin');
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
 // const imagewebp = require('gulp-webp');
 
 // function compilescss() {
@@ -20,23 +15,23 @@ const imagemin = require('gulp-imagemin');
 // }
 
 function jsmin() {
-  return src('src/script/*.js')
-    .pipe(terser())
-    .pipe(dest('public/public/js'))
+  return src("src/script/*.js").pipe(terser()).pipe(dest("public/public/js"));
 }
 
 function optimizeimg() {
-  return src('src/assets*/**/*.{jpg,png,svg}')
-    .pipe(imagemin([
-      imagemin.mozjpeg({
-        quality: 80,
-        progressive: true
-      }),
-      imagemin.optipng({
-        optiminzationLevel: 2
-      }),
-    ]))
-    .pipe(dest('public/public/'))
+  return src("src/assets*/**/*.{jpg,png,svg}")
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({
+          quality: 80,
+          progressive: true,
+        }),
+        imagemin.optipng({
+          optiminzationLevel: 2,
+        }),
+      ])
+    )
+    .pipe(dest("public/public/"));
 }
 
 // function webpImage() {
@@ -47,11 +42,11 @@ function optimizeimg() {
 
 function watchTask() {
   // watch('src/sass/*.scss', compilescss);
-  watch('src/script/*.js', jsmin);
-  watch('src/assets*/**/*.{jpg,png}', optimizeimg);
+  watch("src/script/*.js", jsmin);
+  watch("src/assets*/**/*.{jpg,png}", optimizeimg);
   // watch('public/assets*/**/*.{jpg,png}', webpImage);
 }
 
-exports.default = series(
-  jsmin, optimizeimg, watchTask
-);
+exports.default = series(jsmin, optimizeimg, watchTask);
+
+exports.build = series(jsmin, optimizeimg);
